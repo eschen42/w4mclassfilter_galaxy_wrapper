bf64cd6aff00039568e98cd8b43f08e4d860fb48
Art Eschenlauer <eschen@alumni.princeton.edu>
2018-01-12 09:14:54 +0000

    Docker support for build, test, and deploy

--- run_serve.sh
+++ run_serve.sh
@@ -2,5 +2,5 @@
 
 # run test and write new output files, showing progress to command line output
 planemo conda_install .
-planemo serve --conda_dependency_resolution .
+planemo serve --host 0.0.0.0 --conda_dependency_resolution .
 
